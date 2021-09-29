dae43ebbce95d361d248bcc63645f62fc55b53e8
BY059152 <BY059152@BYMSQ20407.eur.cchbc.com>
2021-09-30 00:29:06 +0300

    Test commit

---
+++ first_task_arrays/src/main/java/by/training/first_task_arrays/App.java
@@ -0,0 +1,17 @@
+package by.training.first_task_arrays;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	static Logger logger = LogManager.getLogger();
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+        logger.error("IOException");
+    }
+}
